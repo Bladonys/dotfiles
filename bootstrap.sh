89649916d51295fce1fcd3799f572c00f25c8cef
Thoams Baminger <thomas.baminger@pm.me>
2025-09-30 15:38:19 +0200

    Add: README

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,22 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
-function doIt() {
+function justDoIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	justDoIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		justDoIt;
 	fi;
 fi;
-unset doIt;
+unset justDoIt;
