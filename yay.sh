418e9879e05fe32fde204769ae285212d830ced7
Thomas Baminger <thomas.baminger@pm.me>
2025-10-09 11:55:47 +0200

    Add: apple-fonts package

--- yay.sh
+++ yay.sh
@@ -15,7 +15,7 @@ echo "Starting system upgrade"
 yay -Syu
 
 # Add or remove packages from here
-packages="base-devel git coreutils moreutils findutils sed bash bash-completion grep openssh php lua 7zip pigz pv tree zopfli python nodejs angular-cli go ghostty zed jdk-temurin legcord element-desktop tidal-hifi-bin obsidian zen-browser-bin google-chrome proton-mail-bin proton-pass-bin jetbrains-toolbox"
+packages="base-devel git coreutils moreutils findutils sed bash bash-completion grep openssh php lua 7zip pigz pv tree zopfli python nodejs angular-cli go ghostty zed jdk-temurin legcord element-desktop tidal-hifi-bin obsidian zen-browser-bin google-chrome proton-mail-bin proton-pass-bin jetbrains-toolbox apple-fonts"
 
 echo "Starting package install"
 yay -Sy --needed --noconfirm $packages
